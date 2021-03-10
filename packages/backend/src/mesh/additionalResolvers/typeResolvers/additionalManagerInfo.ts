6d49010aa689002a091a5f55dd03bbf8b6417d83
Angel Svirkov <angel.svirkov@trivago.com>
2021-03-10 09:40:24 +0100

    chore(backend): Change mock address

--- packages/backend/src/mesh/additionalResolvers/typeResolvers/additionalManagerInfo.ts
+++ packages/backend/src/mesh/additionalResolvers/typeResolvers/additionalManagerInfo.ts
@@ -1,5 +1,5 @@
 import { AdditionalManagerInfoResolvers } from '@src/mesh/types/generated';
 
 export const additionalManagerInfoResolver: AdditionalManagerInfoResolvers = {
-  address: () => 'Kesselstrasse 5 - 7, 40221 Dusseldorf Germany',
+  address: () => 'First Street 4, New Land, Mars',
 };
