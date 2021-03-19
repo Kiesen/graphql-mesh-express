74dbfcf019a36027bfbcc1647f320612c79f9c1a
Angel Svirkov <angel.svirkov@trivago.com>
2021-03-19 14:50:26 +0100

    oos-fix(db): Fix env variable name

--- packages/databases/src/knexfile.ts
+++ packages/databases/src/knexfile.ts
@@ -2,8 +2,8 @@ import knex from 'knex';
 
 import './env';
 
-const DB_PORT = process.env.DB_PORT
-  ? parseInt(process.env.DB_PORT, 10)
+const DB_PORT = process.env.MYSQL_PORT
+  ? parseInt(process.env.MYSQL_PORT, 10)
   : 3306;
 
 const config: knex.Config = {
